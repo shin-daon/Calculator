d31d4715427efefdd7c50449605512f2d3a53068
daon <zzangdaon@gmail.com>
2024-04-26 19:31:15 +0900

    Lv1. 1번 문제 해결

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int firstNum = sc.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int secondNum = sc.nextInt();
+
+    }
+}
