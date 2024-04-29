5e49fa334e3c21fc5cb7e331e4e69663cd2f5f3d
daon <zzangdaon@gmail.com>
2024-04-29 16:45:30 +0900

    Lv2. 7번 문제 해결

--- src/App.java
+++ src/App.java
@@ -3,37 +3,64 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Calculator cal = new Calculator(new ArrayList<>()); // 인스턴스 생성 code 수정
+        Calculator cal = new Calculator(new ArrayList<>(), new ArrayList<>()); // 인스턴스 생성 code 수정
 
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int firstNum = sc.nextInt();
+            System.out.println("-----------------");
+            System.out.println("1. 사칙연산");
+            System.out.println("2. 원의 넓이 구하기");
+            System.out.println("-----------------");
 
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int secondNum = sc.nextInt();
+            System.out.print("원하는 메뉴를 선택해주세요 : ");
+            int start = sc.nextInt();
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.next().charAt(0);
+            if (start == 1) { // 사칙연산
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int firstNum = sc.nextInt();
 
-            System.out.println("결과 : " + cal.calculate(firstNum, secondNum, operator)); // calculate 메소드에 매개변수 넘겨주기
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int secondNum = sc.nextInt();
 
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String removeReq = sc.next();
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operator = sc.next().charAt(0);
 
-            if (removeReq.equals("remove")){
-                cal.removeResult(); // 삭제 메소드로 code 수정
+                System.out.println("결과 : " + cal.calculate(firstNum, secondNum, operator)); // calculate 메소드에 매개변수 넘겨주기
+
+                System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제) ");
+                String removeReq = sc.next();
+
+                if (removeReq.equals("remove")){
+                    cal.removeResult(); // 삭제 메소드로 code 수정
+                }
+
+                System.out.print("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회) ");
+                String inquiryReq = sc.next();
+
+                if (inquiryReq.equals("inquiry")){
+                    cal.inquiryResults(); // 조회 메소드로 code 수정
+                }
+
+            } else if(start == 2){ // 원 넓이 구하기
+                System.out.print("원의 반지름을 입력해주세요 : ");
+                int radius = sc.nextInt();
+
+                System.out.println("원의 넓이 : " + cal.calculateCircleArea(radius));
+
+                System.out.print("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회) ");
+                String inquiryReq = sc.next();
+
+                if (inquiryReq.equals("inquiry")){
+                    cal.inquiryCircles();
+                }
+
+            } else {
+                System.out.println("올바른 메뉴를 선택해주세요.");
+                continue;
             }
 
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String inquiryReq = sc.next();
-
-            if (inquiryReq.equals("inquiry")){
-                cal.inquiryResults(); // 조회 메소드로 code 수정
-            }
-
-            System.out.println("더 계산 하시겠습니까? (exit 입력 시 종료)");
+            System.out.print("더 계산 하시겠습니까? (exit 입력 시 종료) ");
             String exitReq = sc.next();
 
             if (exitReq.equals("exit")){
--- src/Calculator.java
+++ src/Calculator.java
@@ -2,9 +2,20 @@ import java.util.ArrayList;
 
 public class Calculator {
     private ArrayList<Integer> arrList; // 외부에서 직접 접근 불가하도록 수정
+    private ArrayList<Double> circleList; // 원의 넓이 결과를 저장하는 컬렉션 생성
+    public final double PI = 3.14; // final = 하나의 값만 계속 저장해야 하는 변수에 사용하는 예약어 -> 원주율 3.14는 계속 동일 값으로 사용해야하니 final로 지정 후 사용하였음
 
-    public Calculator(ArrayList<Integer> arrList) {
-        this.arrList = new ArrayList<>(); // arrList 초기화
+    public Calculator(ArrayList<Integer> arrList, ArrayList<Double> circleList) { // 생성자 수정
+        this.arrList = arrList;
+        this.circleList = circleList;
+    }
+
+    public ArrayList<Integer> getArrList() { // Getter
+        return arrList;
+    }
+
+    public void setArrList(ArrayList<Integer> arrList) { // Setter
+        this.arrList = arrList;
     }
 
     public int calculate(int firstNum, int secondNum, char operator) {
@@ -35,14 +46,6 @@ public class Calculator {
         return result;
     }
 
-    public ArrayList<Integer> getArrList() { // App class에서 arrList를 가져오기 위해 Getter 생성
-        return arrList;
-    }
-
-    public void setArrList(ArrayList<Integer> arrList) { // Setter
-        this.arrList = arrList;
-    }
-
     public void removeResult() {
         arrList.remove(0);
     }
@@ -50,6 +53,27 @@ public class Calculator {
     public void inquiryResults() {
         for ( Integer list : arrList ) {
             System.out.println("[연산 결과 : " + list + "]");
+        }
+    }
+
+    public double calculateCircleArea(int radius){
+        double result = PI * radius * radius; // 원 넓이 계산
+        circleList.add(result); // 계산 값을 list에 저장
+
+        return result;
+    }
+
+    public ArrayList<Double> getCircleList() { // getter
+        return circleList;
+    }
+
+    public void setCircleList(ArrayList<Double> circleList) { // setter
+        this.circleList = circleList;
+    }
+
+    public void inquiryCircles() {
+        for ( Double list : circleList ) {
+            System.out.println("[원의 넓이 계산 결과 : " + list + "]");
         }
     }
 }
